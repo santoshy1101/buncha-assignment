145c931aadecc6bb5a3d1de523462bf350b98dc1
santosh <styadav018@gmail.com>
2023-09-30 12:01:27 +0530

    buncha asiignemnt

--- src/componenets/navbar/Navbar.jsx
+++ src/componenets/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./navbar.css";
+import "./Navbar.css";
 import { FaFileCsv,FaShoppingCart } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 const Navbar = () => {
